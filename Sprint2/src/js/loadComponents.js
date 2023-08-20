c9705aa9492ff3ed0c94ae9e690661cee5e79638
Raamonvm8 <ravama00@hotmail.com>
2023-08-20 17:53:14 +0100

    añadido media queries y carga dinamica en pagina principal

---
+++ Sprint2/src/js/loadComponents.js
@@ -0,0 +1,11 @@
+function loadComponents(fileName, id, callback) {
+
+    fetch(fileName).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById(id).innerHTML = text;
+        if (callback) {
+            callback();
+        }
+    })
+}
