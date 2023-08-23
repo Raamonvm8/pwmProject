55582d514dd822a9607d14d5569fa9e63afe48a2
Ramón Valls Martin <ravama00@hotmail.com>
2023-08-23 20:38:02 +0100

    Creación y modificación archivos js

---
+++ Sprint2/src/js/comoquieras.js
@@ -0,0 +1,16 @@
+export function showMessage(message, type = "success"){
+    Toastify({
+        text: message,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "right",
+        stopOnFocus: true, 
+        style: {
+          background: type === "success"? "orange":"red",
+          fontWeight: "bold"
+        },
+        onClick: function(){}
+      }).showToast();
+}
--- Sprint2/src/js/validateEmail.js
+++ Sprint2/src/js/validateEmail.js
@@ -1,4 +1,6 @@
-function validateEmail() {
+document.addEventListener('DOMContentLoaded', formularioFunction);
+
+ function validateEmail() {
     // Obtiene el valor del campo de email
     var email = document.getElementsById("email").value;
 
@@ -10,8 +12,52 @@ function validateEmail() {
         // Si el correo no es válido, muestra un mensaje de error
         alert("Correo inválido. Introduce un correo válido.");
     }
-
     return false;
+    
+}
+
+function formularioFunction(event){
+    fetch("../html/login.html").then(res => {
+        
+        return res.text()
+    })
+        .then(texto =>{ 
+            
+            document.getElementById("log").addEventListener("submit", function(event){
+                event.preventDefault()
+               
+            });
+         })
+
+}
+
+function enviarMensaje(e) {
+    e.preventDefault();
+    console.log("Hola, no me estoy recargando");
+  
+}
+
+function lanza_modal() {
+    console.log("holamundo");
+    var email = document.getElementById("email").value;
+
+    // Cambiar logica del validacion
+    if (email.includes("@")) {
+        var usuarioExiste = true;
+        if(usuarioExiste){
+            window.location.href = "initpage.html";
+            //showMessage("Correo correcto", "success")
+
+        }else{
+            alert("Correo no existe. Introduce un correo válido.");
+            
+        }
+
+    }
+
+    
+    
+    
 }
 
 
