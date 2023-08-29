cf95f89ad3accda3b3a5ae365025fb21432a2866
Ramón Valls Martin <ravama00@hotmail.com>
2023-08-29 21:55:28 +0100

    edit validateLogin

--- Sprint2/src/js/validateLogin.js
+++ Sprint2/src/js/validateLogin.js
@@ -2,26 +2,48 @@ function lanza_modal2() {
     // Obtener el valor del campo de correo electrónico y contraseña
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+    
 
     // Cargar el archivo JSON de usuarios
-    fetch("../json/nombres.json")
+    fetch("../assets/json/users.json")
         .then(response => response.json())
         .then(data => {
+            const user = data.users.find(user => user.email === email);
             // Verificar si el correo electrónico existe en el archivo JSON
-            if (!data.includes(email)) {
-                alert("Usuario no encontrado");
-                // Redirigir a la siguiente página en caso de éxito
-                
-            } else if (!data.includes(password)){
+            if (!user) {
+                alert("Usuario no encontrado");                
+            } else if (user.password != password){
                 alert("Contraseña Incorrecta, pruebe de nuevo");
-                
             }else{
-                window.location.href = "initpage.html"; 
+                const userData = {
+                    name: user.name,
+                    surname: user.surname,
+                    email: user.email,
+                    password: user.password,
+                    photo: user.photo,
+                    pets: user.pets,
+                    reservations: user.reservations,
+                    admin: user.admin,
+                };
+                localStorage.setItem('user_data', JSON.stringify(userData));
+                loadHeader(user); // Cargar el header adecuado
+                window.location.href = "initpage.html";
             }
         })
         .catch(error => {
             console.error("Error al cargar el archivo JSON:", error);
         });
+
+
 }
+
+function loadHeader(user) {
+    if (user.admin) {
+        loadComponents("../html/components/headerAdminUser.html", "header");
+    } else {
+        loadComponents("../html/components/headerLoggedUser.html", "header");
+    }
+}
+
 
 
