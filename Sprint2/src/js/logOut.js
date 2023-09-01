c75a6a74b440d5f4d0561c0230e70868b29e9e97
Raamonvm8 <ravama00@hotmail.com>
2023-09-01 11:40:16 +0100

    añadiendo nuevo estilo general

--- Sprint2/src/js/logOut.js
+++ Sprint2/src/js/logOut.js
@@ -1,7 +1,4 @@
 function logOut() {
-    // Elimina los datos del usuario del almacenamiento local
     localStorage.removeItem('user_data');
-
-    // Redirige al usuario a la página de inicio
     window.location.href = "homepage.html";
 }
