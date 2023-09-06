6220d290a696bf19ac374fbacf2b1f44dac43313
Raamonvm8 <ravama00@hotmail.com>
2023-09-06 22:12:28 +0100

    instalacion  android/ios boostrap y sqlite

---
+++ Sprint4/my-pets/src/app/sass.config.js
@@ -0,0 +1,7 @@
+export const includePaths = [
+    "node_modules/ionic-angular/themes",
+    "node_modules/ionicons/scss",
+    "node_modules/ionic-angular/fonts",
+    "node_modules/bootstrap/scss",
+];
+  
--- Sprint4/my-pets/src/app/tabs/tabs.module.ts
+++ Sprint4/my-pets/src/app/tabs/tabs.module.ts
@@ -1,7 +1,10 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 import { TabsPage } from './tabs.page';
 
