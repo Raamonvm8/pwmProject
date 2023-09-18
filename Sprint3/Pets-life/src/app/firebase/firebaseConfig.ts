487989dfe0fd3e669e13a365d55230cf5cb209fa
Ramón Valls Martin <ravama00@hotmail.com>
2023-09-18 20:39:38 +0100

    archivos nuevos

---
+++ Sprint3/Pets-life/src/app/firebase/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export default {
+   apiKey: "AIzaSyDEQizNb5XzqzQdyO-PsyolD50Q5bdIS1o",
+    authDomain: "myapp3-c3cca.firebaseapp.com",
+    projectId: "myapp3-c3cca",
+    storageBucket: "myapp3-c3cca.appspot.com",
+    messagingSenderId: "454349153267",
+    appId: "1:454349153267:web:38e00a652f78fc314de886"
+};
